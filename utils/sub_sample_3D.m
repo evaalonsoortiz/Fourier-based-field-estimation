09fc546be0a2c70d1b8d158c1272844e0f512c30
loreflipts <112189990+loreflipts@users.noreply.github.com>
2022-12-02 11:19:38 -0500

    Add files via upload

--- utils/sub_sample_3D.m
+++ utils/sub_sample_3D.m
@@ -55,7 +55,9 @@ vol_filt = vol_filt(dim_pad + 1: end - dim_pad, dim_pad + 1: end - dim_pad, dim_
 %% Down sampling
 phase = rem(floor(dim / 2)+1, factor); % phase is chosen to be sure to keep the center at the center
 phase(phase == 0) = factor(phase == 0);
+
 vol_lowRes1 = vol_filt(phase(1):factor(1):end, phase(2):factor(2):end, phase(3):factor(3):end);
 vol_lowRes2 = vol_filt(phase(1)+1:factor(1):end, phase(2)+1:factor(2):end, phase(3)+1:factor(3):end);
+
 vol_lowRes = (vol_lowRes1 + vol_lowRes2) ./2;
 end
